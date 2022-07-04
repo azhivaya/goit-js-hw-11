f5db6d4e843ea1f622ca7ca4e7fad4c41e09d781
Alina Zhivaya <zhivayaav@gmail.com>
2022-07-04 10:12:16 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const lightbox = new SimpleLightbox('.gallery a');
 
-
 // import axios from "axios";
 
 
@@ -19,12 +18,10 @@ const form = document.querySelector('#search-form');
 const btnLoadMore = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
 
-// createSimplelightboxGal();
-
 let pageNum = 1;
 let objectValue ='';
 let perPage = 40;
-// const dataForSc = data;
+
 
 // simpleLightbox options
 /* {
@@ -42,16 +39,14 @@ function onFormSubmit(e) {
     e.preventDefault();
 
   objectValue = e.target.elements.searchQuery.value;
-  
-  
-    
   console.log(objectValue);
   form.reset();
   gallery.innerHTML = '';
 
-    fetchImages()
+  fetchImages()
     .then(checkQuery)
     .catch(error => console.log(error));
+  // lightbox.refresh();
 };
 
 function fetchImages() {
@@ -72,6 +67,7 @@ const options = new URLSearchParams({
         }
         return response.json();
     });
+  
 };
 
 function checkQuery(data) {
@@ -82,7 +78,8 @@ if(data.totalHits === 0) {
       };
       Notify.success(`Hooray! We found ${data.totalHits} images on your ${uppercaseQuery} query.`);
         // loadMoreBtnToggle();
-      gallery.insertAdjacentHTML('beforeend', createGallery(data));
+  gallery.insertAdjacentHTML('beforeend', createGallery(data));
+  // lightbox.refresh();
 };
 
 function imagesMarkup({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
@@ -118,106 +115,8 @@ function createGallery({ hits: cards, totalHits: total }) {
   Notify.warning("We're sorry, but you've reached the end of search results.");
       }, 2000)
 
-    }
+  };
+  // lightbox.refresh();
   return cards.map(card => imagesMarkup(card)).join('');
   
 };
-
-// function onBtnLoadMoreClick() {
-//     pageNum +=1;
-
-//   fetchImages().then(data => gallery.insertAdjacentHTML('beforeend', createGallery(data)));
-//   // smoothScrolling();
-  
-// };
-
-// function loadMoreBtnToggle() {
-//     btnLoadMore.classList.toggle('is-hidden');
-// }
-// console.log(newGal);
-
-// const optionsForObserver = {
-//   rootMargin: '200px',
-//   threshold:1.0,
-// };
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       console.log('yeeeehhhh');
-      
-//       // if (!dataForSc.hits.length === 0) {
-//       //   fetchImages().then(data => gallery.insertAdjacentHTML('beforeend', createGallery(dataForSc.hits, dataForSc.totalHits)));
-//       // pageNum += 1;
-//       // }
-//       // return;
-//   }
-// })
-// }, optionsForObserver)
-
-// observer.observe(document.querySelector('.scroll-guard'))
-
-// function smoothScrolling() {
-//   const { height: cardHeight } = document
-//   .querySelector(".gallery")
-//   .firstElementChild.getBoundingClientRect();
-
-//   console.log(cardHeight);
-
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: "smooth",
-// });
-// }
-
-
-// // В ответе будет массив изображений удовлетворивших критериям параметров запроса. Каждое изображение описывается объектом, из которого тебе интересны только следующие свойства:
-
-// // webformatURL - ссылка на маленькое изображение для списка карточек.
-// // largeImageURL - ссылка на большое изображение.
-// // tags - строка с описанием изображения. Подойдет для атрибута alt.
-// // likes - количество лайков.
-// // views - количество просмотров.
-// // comments - количество комментариев.
-// // downloads - количество загрузок.
-
-// function fetchImages(images) {
-// return fetch(url, options).then(r=>r.json()).then(console.log);
-// }
-
-// fetchImages('image').then(image => {console.log(image)})
-// input.addEventListener('input', () => {
-//     search = input.value;
-//     console.log(search);
-// })
-
-// // btn.addEventListener('click', onBtnClick);
-
-// form.addEventListener('submit', onFormSubmit);
-
-// function imagesMarkup(images) {
-// return images.map(({webformatURL,largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
-//   <img src='${webformatURL}' alt="" loading="lazy" />
-//   <div class="info ${tags}">
-//     <p class="info-item">
-//       <b>Likes ${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views ${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments ${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads ${downloads}</b>
-//     </p>
-//   </div>
-// </div>`).join('');
-
-// }
-
-// function addImagesToGallery(images) {
-// gallery.insertAdjacentHTML('beforeend', imagesMarkup());
-// };
-
-// console.log(gallery);
