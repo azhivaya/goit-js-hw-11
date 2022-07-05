ef7e2d2632bf94e710b2429f23acc816729752c4
Alina Zhivaya <zhivayaav@gmail.com>
2022-07-05 12:30:57 +0300

    updates index js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Notify } from "notiflix";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const lightbox = new SimpleLightbox('.gallery a');
+let lightbox = new SimpleLightbox('.photo-card a');
 
 // import axios from "axios";
 
@@ -39,55 +39,52 @@ function onFormSubmit(e) {
     e.preventDefault();
 
   objectValue = e.target.elements.searchQuery.value;
-  console.log(objectValue);
-  form.reset();
-  gallery.innerHTML = '';
+    console.log(objectValue);
+    
+    resetValues();
 
-  fetchImages()
+    fetchImages()
     .then(checkQuery)
     .catch(error => console.log(error));
-  // lightbox.refresh();
 };
 
-function fetchImages() {
-const options = new URLSearchParams({
-    q: objectValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    per_page: perPage,
-    page: pageNum,
-});
-
-    
-    return fetch(`${BASE_URL}?key=${API_KEY}&${options}`)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(response.status)
-        }
-        return response.json();
-    });
-  
-};
-
-function checkQuery(data) {
+function checkQuery({hits: cards, totalHits: totalCards}) {
   const uppercaseQuery = objectValue.toUpperCase();
-if(data.totalHits === 0) {
+// if(data.totalHits === 0) {
+    if(!totalCards){
           Notify.failure(`Sorry, there are no images matching your ${uppercaseQuery} query. Please try again.`);
           return;
       };
-      Notify.success(`Hooray! We found ${data.totalHits} images on your ${uppercaseQuery} query.`);
+      Notify.success(`Hooray! We found ${totalCards} images on your ${uppercaseQuery} query.`);
         // loadMoreBtnToggle();
-  gallery.insertAdjacentHTML('beforeend', createGallery(data));
-  // lightbox.refresh();
+  gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(cards));
+  lightbox.refresh();
 };
+
+
+
+// function btnLoadMoreSwitch() {
+//     if(perPage * pageNum >= total) {
+//       // loadMoreBtnToggle();
+//       setTimeout(() => {
+//   Notify.warning("We're sorry, but you've reached the end of search results.");
+//       }, 2000)
+
+//   };
+  
+// };
+
+function createGalleryMarkup(cards) {
+return cards.map(card => imagesMarkup(card)).join('');
+}
 
 function imagesMarkup({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
   return `
 
 <div class="photo-card">
 <a class='gallery__link href='${largeImageURL}'>
-<img class='gallery__image' src='${webformatURL}' alt="${tags}" loading="lazy" /></a>
+<img class='gallery__image' src='${webformatURL}' alt="${tags}" loading="lazy" />
+</a>
 
   <div class="info">
     <p class="info-item">
@@ -108,15 +105,131 @@ function imagesMarkup({webformatURL, largeImageURL, tags, likes, views, comments
 `
 };
 
-function createGallery({ hits: cards, totalHits: total }) {
-    if(perPage * pageNum >= total) {
-      // loadMoreBtnToggle();
-      setTimeout(() => {
-  Notify.warning("We're sorry, but you've reached the end of search results.");
-      }, 2000)
+function resetValues() {
+    btnLoadMore.classList.add('is-hidden');
+    form.reset();
+    gallery.innerHTML = '';
+    pageNum =1;
+}
 
-  };
-  // lightbox.refresh();
-  return cards.map(card => imagesMarkup(card)).join('');
-  
+function fetchImages() {
+const options = new URLSearchParams({
+    q: objectValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: perPage,
+    page: pageNum,
+});
+
+    
+    return fetch(`${BASE_URL}?key=${API_KEY}&${options}`)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json();
+    });
 };
+
+// function onBtnLoadMoreClick() {
+//     pageNum +=1;
+
+//   fetchImages().then(data => gallery.insertAdjacentHTML('beforeend', createGallery(data)));
+//   // smoothScrolling();
+  
+// };
+
+// function loadMoreBtnToggle() {
+//     btnLoadMore.classList.toggle('is-hidden');
+// }
+// console.log(newGal);
+
+// const optionsForObserver = {
+//   rootMargin: '200px',
+//   threshold:1.0,
+// };
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       console.log('yeeeehhhh');
+      
+//       // if (!dataForSc.hits.length === 0) {
+//       //   fetchImages().then(data => gallery.insertAdjacentHTML('beforeend', createGallery(dataForSc.hits, dataForSc.totalHits)));
+//       // pageNum += 1;
+//       // }
+//       // return;
+//   }
+// })
+// }, optionsForObserver)
+
+// observer.observe(document.querySelector('.scroll-guard'))
+
+// function smoothScrolling() {
+//   const { height: cardHeight } = document
+//   .querySelector(".gallery")
+//   .firstElementChild.getBoundingClientRect();
+
+//   console.log(cardHeight);
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: "smooth",
+// });
+// }
+
+
+// // В ответе будет массив изображений удовлетворивших критериям параметров запроса. Каждое изображение описывается объектом, из которого тебе интересны только следующие свойства:
+
+// // webformatURL - ссылка на маленькое изображение для списка карточек.
+// // largeImageURL - ссылка на большое изображение.
+// // tags - строка с описанием изображения. Подойдет для атрибута alt.
+// // likes - количество лайков.
+// // views - количество просмотров.
+// // comments - количество комментариев.
+// // downloads - количество загрузок.
+
+// function fetchImages(images) {
+// return fetch(url, options).then(r=>r.json()).then(console.log);
+// }
+
+// fetchImages('image').then(image => {console.log(image)})
+// input.addEventListener('input', () => {
+//     search = input.value;
+//     console.log(search);
+// })
+
+// // btn.addEventListener('click', onBtnClick);
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function imagesMarkup(images) {
+// return images.map(({webformatURL,largeImageURL, tags, likes, views, comments, downloads}) => `<div class="photo-card">
+//   <img src='${webformatURL}' alt="" loading="lazy" />
+//   <div class="info ${tags}">
+//     <p class="info-item">
+//       <b>Likes ${likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views ${views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments ${comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads ${downloads}</b>
+//     </p>
+//   </div>
+// </div>`).join('');
+
+// }
+
+// function addImagesToGallery(images) {
+// gallery.insertAdjacentHTML('beforeend', imagesMarkup());
+// };
+
+// console.log(gallery);
+
+
+
--- src/partials/lightbox.js
+++
@@ -1,5 +0,0 @@
-
-
-export function createSimplelightboxGal() {
-    const simpleLightbox = new SimpleLightbox('.gallery a');
-}
